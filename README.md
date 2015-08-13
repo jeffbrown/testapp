57deef1ea2b76c82cb9d53a6d682c3030e481641
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-08-13 04:23:37 -0500

    simplify problematic scenario

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 To make a call to the test app and test the error:
 
-curl --verbose --request GET --header "Content-Type: application/json" "http://localhost:8080/api_v0.1/post/show" --cookie cookies.txt
-
+curl "http://localhost:8080/api_v0.1/post/show"
--- grails-app/controllers/TestappInterceptor.groovy
+++ grails-app/controllers/TestappInterceptor.groovy
@@ -1,39 +1,11 @@
-
-
-
-import javax.servlet.http.HttpServletResponse
-
 class TestappInterceptor{
 
-	int order = HIGHEST_PRECEDENCE + 999
-
-    String entrypoint = "api_v0.1"
-
-    def apiResponseService
-
     def TestappInterceptor() {
-
-        match(uri:/^(\/${entrypoint}\/*(.+))$/)
-	}
-
-	boolean before(){
-        return true
+		match controller: 'post'
 	}
 
 	boolean after(){
-        println(model.getClass())
-		try{
-			if(!model){
-				render(status:HttpServletResponse.SC_BAD_REQUEST)
-			}else{
-                def newModel = (model)?apiResponseService.convertModel(model):model
-                render(text:newModel, contentType:"text/json", encoding:"UTF-8")
-           }
-	   }catch(Exception e){
-		   log.error("[ApiToolkitFilters :: apitoolkit.after] : Exception - full stack trace follows:", e);
-		   return false
-	   }
-
-	   return false
+		render 'test'
+		false
 	}
 }
