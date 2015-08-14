8c3c8be0c37282d3ebb8c3bc5e74b48f693a3d16
Owen Rubel <orubel@gmail.com>
2015-08-14 06:23:51 -0700

    Update TestappInterceptor.groovy

--- grails-app/controllers/TestappInterceptor.groovy
+++ grails-app/controllers/TestappInterceptor.groovy
@@ -1,11 +1,21 @@
+import javax.servlet.http.HttpServletResponse
+
 class TestappInterceptor{
 
+    String entrypoint = "api_v0.1"
+
+    // Test with URI; less common usage case
     def TestappInterceptor() {
-		match controller: 'post'
+        match(uri:/^(\/${entrypoint}\/*(.+))$/)
+    }
+
+	boolean before(){
+            return true
 	}
 
+        // render full api return per request
 	boolean after(){
-		render 'test'
-		false
+            render(text:model, contentType:"text/json", encoding:"UTF-8")
+	    return false
 	}
 }
