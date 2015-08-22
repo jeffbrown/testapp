34ab08025a5e5989bc468271f1c52b7c58e27688
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-08-22 09:17:32 -0500

    test scenario of several redirect round trips

--- src/integration-test/groovy/testapp/MappingConfigFunctionalSpec.groovy
+++ src/integration-test/groovy/testapp/MappingConfigFunctionalSpec.groovy
@@ -20,6 +20,20 @@ class MappingConfigFunctionalSpec extends GebSpec {
         $().text() == 'Message: [(beforeInterceptor) (afterInterceptor) (id is 1) (model : [fname:Bob]) (redirect) (beforeInterceptor) (afterInterceptor) (id is 0)]'
     }
 
+    void 'test invoking a URL that results in several round trips of interceptor redirects'() {
+        when:
+        go '/testapp_v0.1/post/show/2'
+
+        then:
+        $().text() == 'rendered by TestappInterceptor.after'
+
+        when:
+        go '/testapp_v0.1/message'
+
+        then:
+        $().text() == 'Message: [(beforeInterceptor) (afterInterceptor) (id is 2) (model : [fname:Bob]) (redirect) (beforeInterceptor) (afterInterceptor) (id is 1) (model : [fname:Bob]) (redirect) (beforeInterceptor) (afterInterceptor) (id is 0)]'
+    }
+
     void 'test that interceptors are not run when a request is sent to a URL that does not match a URL mapping'() {
         when:
         go '/fred/post/show/1'
